f59ef0184aab81390e59e8d4ae54c489967ee276
Alex Anderson <alexanderson1993@gmail.com>
2018-09-04 17:00:25 -0600

    Update to fix the broken widget.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,23 +1,3 @@
 import React from 'react'
 import CMS from 'netlify-cms'
 import 'netlify-cms/dist/cms.css'
-
-const Category = ({ value = 'nothing', onChange }) => {
-  return (
-    <div>
-      <select value={value} onChange={e => onChange(e.target.value)}>
-        <option value="nothing" disabled>
-          Select a category
-        </option>
-        <option value="docs">General Documentation</option>
-        <option value="cards">Cards</option>
-        <option value="flight_directing">Flight Directing</option>
-        <option value="simulator_config">Simulator Configuration</option>
-        <option value="mission_writing">Mission Writing</option>
-        <option value="developers">Developers</option>
-      </select>
-    </div>
-  )
-}
-
-CMS.registerWidget('category', Category)
