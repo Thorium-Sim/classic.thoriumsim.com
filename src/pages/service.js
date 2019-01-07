d01c3ad45aee2ad1f28b8d827137a244d3187d12
Alex Anderson <alexanderson1993@gmail.com>
2019-01-07 09:43:31 -0700

    Update the rates on the service form.

--- src/pages/service.js
+++ src/pages/service.js
@@ -47,17 +47,17 @@ const Select = Input.withComponent("select");
 
 const priorityOptions = {
   Feature: [
-    "Low - Complete the feature within one month. $15/hr",
-    "Medium - Complete the feature within two weeks. $25/hr",
-    "High - Complete the feature within one week. $50/hr"
+    "Low - Complete the feature within one month. $20/hr",
+    "Medium - Complete the feature within two weeks. $30/hr",
+    "High - Complete the feature within one week. $60/hr"
   ],
   "Bug Fix": [
-    "Low - Fix the bug within one week. $15/hr",
-    "Medium - Fix the bug within one day. $25/hr",
-    "High - Fix the bug within four hours. $50/hr"
+    "Low - Fix the bug within one week. $25/hr",
+    "Medium - Fix the bug within one day. $35/hr",
+    "High - Fix the bug within four hours. $65/hr"
   ],
   "On-Site Visit": [
-    "Low - Arrive at the location within one week. Good for getting Thorium set up. $25/hr",
+    "Low - Arrive at the location within one week. Good for getting Thorium set up. $30/hr",
     "Medium - Arrive at the location within two working days. $50/hr",
     "High - Arrive at the location within two hours. Mission-critical issues. $100/hr"
   ]
@@ -235,6 +235,16 @@ const Service = () => {
             hurry? How about a bug that needs to be fixed quickly? Need someone
             to come on site to help you get everything working right? Then fill
             out this form, and we'll make it happen!
+          </p>
+          <p>
+            <strong>Note:</strong>
+            <em>
+              As of January 2019, the rates for the service request have
+              changed. Please review the options before submitting a request. If
+              you have any questions or want to work out a custom rate, please
+              reach out at{" "}
+              <a href="mailto:alex@fyreworks.us">alex@fyreworks.us</a>
+            </em>
           </p>
           <Form />
           <h2>Frequently Asked Questions</h2>
